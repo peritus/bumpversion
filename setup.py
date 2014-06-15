0cca078ee61705bbff590ed9c61da7d36d7744c5
Filip Noetzel <filip@j03.de>
2014-06-15 05:46:28 -0700

    Bump version: 0.4.1 → 0.5.0-dev

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ description = 'Version-bump your software with a single command!'
 
 setup(
     name='bumpversion',
-    version='0.4.1',
+    version='0.5.0-dev',
     url='https://github.com/peritus/bumpversion',
     author='Filip Noetzel',
     author_email='filip+bumpversion@j03.de',
